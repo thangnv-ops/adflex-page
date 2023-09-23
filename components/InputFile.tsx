4535a82ba9c7aa2e6d05abd34b9064710172b904
Robert Nguyen <robert@travala.com>
2023-09-23 20:46:26 +0700

    feat: test

--- components/InputFile.tsx
+++ components/InputFile.tsx
@@ -7,7 +7,7 @@ function InputFile({ className = '' }: { className?: string }) {
       <div className="flex flex-col items-center px-6 py-4 text-center bg-white rounded-lg">
         <UploadIcon />
         <p className="text-[#3355A6] font-medium mt-3 text-sm">Click để upload CV</p>
-        <p className="text-[#667085] text-xs mt-1">PDF, DOC, DOCX (tối đa 5MB)</p>
+        <p className="text-[#667085] text-xs mt-1">PDF, DOC, DOCX (tối đa 4MB)</p>
       </div>
       <input className="hidden" type="file" />
     </label>
